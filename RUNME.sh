79be9d69c2385ebe5b66ed78fc25ee9047f45fe1
Antti Järvinen <aj750226@gmail.com>
2020-10-04 18:16:41 +0300

    Add script to replace all template strings

---
+++ RUNME.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter repository name (eg. unity-simple-notifications): " REPOSITORY_NAME
+read -p "Enter friendly name (eg. Notifications): " FRIENDLY_NAME
+read -p "Enter description (eg. Simple UI notifications for Unity game engine.): " DESCRIPTION
+read -p "Enter unity version (eg. 2019.4): " UNITY_VERSION
+
+echo 'Replacing template strings...'
+
+( shopt -s globstar dotglob;
+    for file in **; do
+        if [[ -f $file ]] && [[ -w $file ]]; then
+            sed -i -- 's/{{REPOSITORY_NAME}}/"$REPOSITORY_NAME"/g;s/{{FRIENDLY_NAME}}/"$FRIENDLY_NAME"/g;s/{{DESCRIPTION}}/"$DESCRIPTION"/g;s/{{UNITY_VERSION}}/"$UNITY_VERSION"/g' "$file"
+        fi
+    done
+)
+
+echo 'done.'
