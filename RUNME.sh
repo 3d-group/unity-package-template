5cd1688630b4e00f91812a7c68d86e708842cb21
Antti Järvinen <aj750226@gmail.com>
2020-10-05 12:18:35 +0300

    Add more company related tags. Closes #4

--- RUNME.sh
+++ RUNME.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-read -e -p "Enter company name: " -i "3D Group" COMPANY_FRIENDLY_NAME
-read -e -p "Enter company name in lower case: " -i "3d-group" COMPANY
-read -e -p "Enter repository name: " -i "unity-simple-notifications" REPOSITORY_NAME
-read -e -p "Enter friendly name: " -i "Notifications" FRIENDLY_NAME
-read -e -p "Enter description: " -i "Package for Unity game engine." DESCRIPTION
-read -e -p "Enter unity version: " -i "2019.4" UNITY_VERSION
+read -e -p "Company name: " -i "3D Group" COMPANY_FRIENDLY_NAME
+read -e -p "Company name in lower case: " -i "3d-group" COMPANY
+read -e -p "Unacceptable behavior may be reported at: " -i "reports@3d.fi" REPORT_EMAIL
+read -e -p "Company website: " -i "https://3d.fi" COMPANY_WEBSITE
+read -e -p "Repository name: " -i "unity-simple-notifications" REPOSITORY_NAME
+read -e -p "Friendly name: " -i "Notifications" FRIENDLY_NAME
+read -e -p "Description: " -i "Package for Unity game engine." DESCRIPTION
+read -e -p "Unity version: " -i "2019.4" UNITY_VERSION
 
 echo 'Replacing template strings...'
 
@@ -15,7 +17,7 @@ YEAR="$(date +'%Y')"
     for file in **; do
         if [[ -f $file ]] && [[ -w $file ]] && [[ $file != 'RUNME.sh' ]] && [[ $file != Samples/** ]] && [[ $file != .git/** ]]; then
 		    echo "Altering file ${file}"
-            replace="s/{{REPOSITORY_NAME}}/"${REPOSITORY_NAME}"/g;s/{{FRIENDLY_NAME}}/"${FRIENDLY_NAME}"/g;s/{{DESCRIPTION}}/"${DESCRIPTION}"/g;s/{{UNITY_VERSION}}/"${UNITY_VERSION}"/g;s/{{COMPANY}}/"${COMPANY}"/g;s/{{COMPANY_FRIENDLY_NAME}}/"${COMPANY_FRIENDLY_NAME}"/g;s/{{YEAR}}/"${YEAR}"/g"
+            replace="s/{{REPOSITORY_NAME}}/"${REPOSITORY_NAME}"/g;s/{{FRIENDLY_NAME}}/"${FRIENDLY_NAME}"/g;s/{{DESCRIPTION}}/"${DESCRIPTION}"/g;s/{{UNITY_VERSION}}/"${UNITY_VERSION}"/g;s/{{COMPANY}}/"${COMPANY}"/g;s/{{COMPANY_FRIENDLY_NAME}}/"${COMPANY_FRIENDLY_NAME}"/g;s/{{YEAR}}/"${YEAR}"/g;s/{{COMPANY_WEBSITE}}/"${COMPANY_WEBSITE}"/g;s/{{REPORT_EMAIL}}/"${REPORT_EMAIL}"/g"
 			
 			# Replace template strings inside files
 			sed -i -- $replace "$file"
@@ -31,9 +33,11 @@ echo 'done.'
 
 # Remove template repository specific files
 rm README.md
+rm CONTRIBUTING.md
 rm LICENSE
 rm package.json
 mv example-README.md README.md
+mv example-CONTRIBUTING.md CONTRIBUTING.md
 mv example-LICENSE LICENSE
 mv example-package.json package.json
 mv example-.github .github
--- example-CONTRIBUTING.md
+++ example-CONTRIBUTING.md
@@ -2,7 +2,7 @@
 
 We use fairly "standard" github contribution workflow:
 
-1. Make an [Issue](https://github.com/3d-group/unity-package-template/issues/new)
+1. Make an [Issue](https://github.com/{{COMPANY}}/{{REPOSITORY_NAME}}/issues/new)
 2. Fork this repository (For further details, see https://docs.github.com/en/github/getting-started-with-github/fork-a-repo)
 3. Develop changes to a new branch to your forked repository
 4. Create a Pull Request from your forked repository against this repository
@@ -10,6 +10,23 @@ We use fairly "standard" github contribution workflow:
    2. Pull request description should answer these questions: "What has been changed" and "What is this for"
 
 Please note we have a code of conduct, please follow it in all your interactions with the project :smile:
+
+## Developing
+
+One way to develop this Unity package is to create a new Unity Project and copy this package to its Assets folder.
+
+This way .meta files (required by Unity) are generated automatically. Assets available in the package can now be tested and developed inside the project.
+
+After making changes you can test your package by eg. installing it via Git URL:
+
+Open `Packages/manifest.json` with your favorite text editor. Add following line to the dependencies block:
+```json
+    {
+        "dependencies": {
+            "com.{{COMPANY}}.{{REPOSITORY_NAME}}": "https://github.com/YOUR_USER/{{REPOSITORY_NAME}}.git"
+        }
+    }
+```
 
 For further details, see [Unity docs about custom packages](https://docs.unity3d.com/Manual/CustomPackages.html).
 
@@ -74,7 +91,7 @@ representative at an online or offline event.
 ## Enforcement
 
 Instances of abusive, harassing, or otherwise unacceptable behavior may be
-reported to the community leaders responsible for enforcement at reports@3d.fi.
+reported to the community leaders responsible for enforcement at {{REPORT_EMAIL}}.
 All complaints will be reviewed and investigated promptly and fairly.
 
 All community leaders are obligated to respect the privacy and security of the
